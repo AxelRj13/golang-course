70a9d14d2625d575b62ec8f13a92d7ca7e8a5341
AxelRj13 <axel.raharja1@gmail.com>
2024-04-03 00:54:34 +0700

    channels and routines

--- channels/main.go
+++ channels/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -14,17 +15,33 @@ func main() {
 		"http://amazon.com",
 	}
 
+	// create channel for main routine to be able to communicate with its child routine(s)
+	c := make(chan string)
+
 	for _, link := range links {
-		go.checkLink(link)
+		go checkLink(link, c) // create child routine each time this func is called
+	}
+
+	// for { // endless / infinite loop
+	// 	// fmt.Println(<-c) // wait for a value to be sent into the channel, then log it out
+	// 	go checkLink(<-c, c) // waiting for message then directly call the function and pass the URL back
+	// }
+
+	for l := range c { // wait channel to return value then assign it to variable l then starts the loop
+		go func(link string) { // function literal
+			time.Sleep(time.Second * 5) // sleep routine for every call
+			checkLink(link, c)
+		}(l)
 	}
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, error := http.Get(link)
 	if error != nil {
 		fmt.Println(link, "might be down!")
+		c <- link //send message to channel
 		return
 	}
-
 	fmt.Println(link, "is up!")
+	c <- link
 }
